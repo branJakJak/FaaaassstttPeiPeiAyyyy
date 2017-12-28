7f7a9750e8e3bcd60a4df7c58822eeff715b044b
axis161 <axis161@site8.co>
2017-12-29 00:15:21 +0800

    UPDATE = change db config

--- config/db.php
+++ config/db.php
@@ -6,7 +6,6 @@ return [
     'username' => getenv('DATABASE_USERNAME'),
     'password' => getenv('DATABASE_PASSWORD'),
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
